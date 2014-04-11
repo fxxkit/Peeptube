3033b6ddb8e212e41dea3e38d0f2914fc8889e13
maxis <maxis1718@gmail.com>
2014-04-11 18:45:25 +0800

    distribution

---
+++ scripts/fff66610.main.js
@@ -0,0 +1,1 @@
+function events(){"use strict";$(".nav").find("li").click(function(){$(this).siblings().removeClass("active"),$(this).addClass("active")})}function onReady(){"use strict";events()}$(document).ready(onReady);
